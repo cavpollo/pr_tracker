28ed9c503f3fa389a20a17fb6cdd11e392e692ab
Cavpollo <cavpollo@gmail.com>
2017-10-19 15:37:35 -0600

    Obtain Repositories, Pull Requests, and most Pull Request data

--- background.js
+++ background.js
@@ -1,5 +1,11 @@
 var pollIntervalMinutesMin = 1;
 var pollIntervalMinutesMax = 60;
+var requestTimeoutSeconds = 1000 * 2;
+var repositoriesData = [];
+
+var LOADING = 'LOADING';
+var LOADED = 'LOADED';
+var ERROR = 'ERROR';
 
 function goToIndexPage() {
     var indexUrl = chrome.runtime.getURL("index.html");
@@ -39,18 +45,258 @@ function loadData() {
         authToken: '',
         organization: ''
     }, function (items) {
-        items.authToken;
-        items.organization;
+        var authToken = items.authToken;
+        var organization = items.organization;
 
-        if (items.authToken && items.organization) {
-
+        if (authToken && organization) {
+            getRepositories(authToken, organization);
         } else {
             console.log("No oauth token or organization");
             delete localStorage.notificationCount;
+            updateIcon();
         }
-
-        updateIcon();
     });
+}
+
+function getRepositories(authToken, organization) {
+    var xhr = new XMLHttpRequest();
+
+    var abortTimerId = window.setTimeout(function () {
+        xhr.abort();
+    }, requestTimeoutSeconds);
+
+    function handleError() {
+        window.clearTimeout(abortTimerId);
+        localStorage.requestFailureCount++;
+
+        delete localStorage.notificationCount;
+        updateIcon();
+    }
+
+    try {
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+
+            if (this.status < 200 || this.status >= 300) {
+                console.error('Get Repositories Failed: ' + this.status);
+                return;
+            }
+
+            if (xhr.responseText) {
+                var repositoryArray = JSON.parse(xhr.responseText);
+                // console.log(repositoryArray);
+
+                repositoriesData = [];
+                for (var i = 0, repository; repository = repositoryArray[i]; i++) {
+                    var repoData = {
+                        name: repository.name,
+                        full_name: repository.full_name,
+                        git_url: repository.git_url,
+                        updated_at: repository.updated_at,
+                        pull_requests: [],
+                        status: LOADING
+                    };
+                    repositoriesData.push(repoData);
+
+                    updateIcon();
+
+                    getRepositoryPullRequests(authToken, repoData);
+                }
+
+                return;
+            }
+
+            handleError();
+        };
+        xhr.onerror = function (error) {
+            handleError();
+        };
+        xhr.open("GET", "https://api.github.com/orgs/" + organization + "/repos", true);
+        xhr.setRequestHeader("Authorization", "token " + authToken);
+        xhr.send(null);
+    } catch (e) {
+        console.error("Get Repositories XHR exception");
+        handleError();
+    }
+}
+
+function getRepositoryPullRequests(authToken, repoData) {
+    var xhr = new XMLHttpRequest();
+    var repositoryFullName = repoData.full_name;
+
+    var abortTimerId = window.setTimeout(function () {
+        xhr.abort();
+    }, requestTimeoutSeconds);
+
+    function handleError() {
+        window.clearTimeout(abortTimerId);
+        localStorage.requestFailureCount++;
+
+        repoData.status = ERROR;
+
+        delete localStorage.notificationCount;
+        updateIcon();
+    }
+
+    try {
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+
+            if (this.status < 200 || this.status >= 300) {
+                console.error('Get Repositories Failed: ' + this.status);
+                repoData.status = ERROR;
+                return;
+            }
+
+            if (xhr.responseText) {
+                var pullRequestArray = JSON.parse(xhr.responseText);
+                if (pullRequestArray.length > 0) {
+                    console.log(repositoryFullName + ':');
+                    console.log(pullRequestArray);
+                }
+
+                for (var i = 0, pullRequest; pullRequest = pullRequestArray[i]; i++) {
+                    var assigneesArray = [];
+                    for (var j = 0, assignee; assignee = pullRequest.assignees[j]; j++) {
+                        var assigneeData = {
+                            id: assignee.id,
+                            username: assignee.login,
+                            avatar_url: assignee.avatar_url,
+                            url: assignee.html_url
+                        };
+                        assigneesArray.push(assigneeData);
+                    }
+
+                    var pendingReviewers = [];
+                    for (var k = 0, reviewer; reviewer = pullRequest.requested_reviewers[k]; k++) {
+                        var pendingReviewerData = {
+                            id: reviewer.id,
+                            username: reviewer.login,
+                            avatar_url: reviewer.avatar_url,
+                            url: reviewer.html_url
+                        };
+                        pendingReviewers.push(pendingReviewerData);
+                    }
+
+
+                    var pullRequestData = {
+                        id: pullRequest.id,
+                        number: pullRequest.number,
+                        title: pullRequest.title,
+                        url: pullRequest.html_url,
+                        assignees: assigneesArray,
+                        head_name: pullRequest.head.ref,
+                        base_name: pullRequest.base.ref,
+                        tags: [],
+                        pending_reviewers: pendingReviewers,
+                        disapproved_reviewers: [],
+                        approved_reviewers: [],
+                        unanswered_comments: -1,
+                        comments: -1,
+                        commits: -1,
+                        additions: -1,
+                        deletions: -1,
+                        changed_files: -1,
+                        mergeable: null,
+                        created_at: pullRequest.created_at,
+                        updated_at: pullRequest.updated_at,
+                        status: LOADING
+                    };
+
+                    repoData.pull_requests.push(pullRequestData);
+
+                    //pullRequest.review_comments_url
+                    //pullRequest.comments_url
+
+                    updateIcon();
+
+                    getPullRequest(authToken, repositoryFullName, pullRequestData);
+
+                    // Add tags
+                    // Count how many comments are left unanswered by the assignee
+                    // Add the approved & disapproved reviewers data (name, picture, and status)
+                }
+                repoData.status = LOADED;
+
+                return;
+            }
+
+            handleError();
+        };
+        xhr.onerror = function (error) {
+            handleError();
+        };
+        xhr.open("GET", "https://api.github.com/repos/" + repositoryFullName + "/pulls?state=open&sort=created&direction=asc", true);
+        xhr.setRequestHeader("Authorization", "token " + authToken);
+        xhr.send(null);
+    } catch (e) {
+        console.error("Get Repositories XHR exception");
+        handleError();
+    }
+}
+
+function getPullRequest(authToken, repositoryFullName, pullRequestData) {
+    var xhr = new XMLHttpRequest();
+    var pullRequestNumber = pullRequestData.number;
+
+    var abortTimerId = window.setTimeout(function () {
+        xhr.abort();
+    }, requestTimeoutSeconds);
+
+    function handleError() {
+        window.clearTimeout(abortTimerId);
+        localStorage.requestFailureCount++;
+
+        pullRequestData.status = ERROR;
+
+        delete localStorage.notificationCount;
+        updateIcon();
+    }
+
+    try {
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState !== 4) {
+                return;
+            }
+
+            if (this.status < 200 || this.status >= 300) {
+                console.error('Get Repositories Failed: ' + this.status);
+                pullRequestData.status = ERROR;
+                return;
+            }
+
+            if (xhr.responseText) {
+                var pullRequestInfo = JSON.parse(xhr.responseText);
+                console.log(repositoryFullName  + '/' + pullRequestNumber + ':');
+                console.log(pullRequestInfo);
+
+                pullRequestData.comments = pullRequestInfo.comments;
+                pullRequestData.commits = pullRequestInfo.commits;
+                pullRequestData.additions = pullRequestInfo.additions;
+                pullRequestData.deletions = pullRequestInfo.deletions;
+                pullRequestData.changed_files = pullRequestInfo.changed_files;
+                pullRequestData.mergeable = pullRequestInfo.mergeable;
+                pullRequestData.status = LOADED;
+
+                return;
+            }
+
+            handleError();
+        };
+        xhr.onerror = function (error) {
+            handleError();
+        };
+        xhr.open("GET", "https://api.github.com/repos/" + repositoryFullName + "/pulls/" + pullRequestNumber, true);
+        xhr.setRequestHeader("Authorization", "token " + authToken);
+        xhr.send(null);
+    } catch (e) {
+        console.error("Get Repositories XHR exception");
+        handleError();
+    }
 }
 
 function onAlarm(alarm) {
